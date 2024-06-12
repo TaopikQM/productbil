9321ce909c0b03a4f77b4b2a42117f66a79263da
TaopikQM <taopik61318@gmail.com>
2024-06-12 07:36:11 +0700

    Update Navbar.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white dark:bg-gray-200 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <Link href="/" passHref>
           <div className="flex items-center space-x-3 rtl:space-x-reverse cursor-pointer">

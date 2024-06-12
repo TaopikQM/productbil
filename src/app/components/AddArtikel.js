803981f2c25476b06c07fead5024a91283cba130
TaopikQM <taopik61318@gmail.com>
2024-06-12 07:31:07 +0700

    Update AddArtikel.js

--- src/app/components/AddArtikel.js
+++ src/app/components/AddArtikel.js
@@ -112,7 +112,7 @@ const AddArtikel = () => {
           disabled={isLoading} // Tambahkan properti disabled berdasarkan isLoading
           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
-         {isLoading ? "Adding..." : "Add Wisata"} {/* Ubah label tombol berdasarkan isLoading */}
+         {isLoading ? "Adding..." : "Add"} {/* Ubah label tombol berdasarkan isLoading */}
      
         </button>
 

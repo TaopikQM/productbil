fe29e76c85af16111a729105db13662e8030f343
TaopikQM <taopik61318@gmail.com>
2024-06-12 07:11:18 +0700

    Add files via upload

---
+++ src/app/components/containerAw.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Box from "./Box";
+
+export const ContainerAw = ({ children }) => {
+  return (
+    <div className="w-full min-h-screen bg-[#f3f3f3] flex justify-center items-start py-10">
+      <div className="w-full max-w-7xl bg-white p-10 rounded-lg shadow-lg">
+        {children}
+       
+      </div>
+    </div>
+  );
+};
+
+export default ContainerAw;

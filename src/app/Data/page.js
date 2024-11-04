47f1b5bf0a4e387196d20776f242771452e38ea9
TaopikQM <taopik61318@gmail.com>
2024-11-05 00:35:00 +0700

    Update page.js

--- src/app/Data/page.js
+++ src/app/Data/page.js
@@ -7,7 +7,7 @@ import ContainerAw from '../components/containerAw';
 
 import Navbar from '../components/Navbar';
 
-import AddFiles from '../components/Addfiles';
+import AddFiles from '../components/AddFiles';
 import FilesList from '../components/FilesList';
 
 
@@ -32,17 +32,7 @@ const addFiles = () => {
           <FilesList/>
        
         </ContainerAw>
-     // <Navbar/>
-     //    <ContainerAw>
-
-     //        <div className="flex items-center justify-center">
-     //            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={toggleForm}>Add Aksi</button>
-     //        </div>
-     //        {showForm && <AddAksi />}
-     //        <br/>
-     //      <AksiList/>
-       
-     //    </ContainerAw>
+   
    </div>
   );
 };

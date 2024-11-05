bfbfbbfbb4f02d4a3970c9115d242e9eb8f7e1b9
TaopikQM <taopik61318@gmail.com>
2024-11-05 12:49:20 +0700

    Update containerAw.js

--- containerAw.js
+++ containerAw.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const ContainerAw = ({ children, isDayMode }) => {
   return (
-    <div className={`w-full min-h-screen flex justify-center items-start py-10 ${isDayMode ? 'bg-[#f3f3f3]' : 'bg-gray-800'}`}>
-      <div className={`w-full max-w-7xl p-10 rounded-lg shadow-lg ${isDayMode ? 'bg-white' : 'bg-gray-900 text-white'}`}>
+    <div className={`w-full min-h-screen flex justify-center items-start py-10 isDayMode ? 'bg-white text-black' : 'bg-gray-800 text-white`>
+      <div className={`w-full max-w-7xl p-10 rounded-lg shadow-lg isDayMode ? 'bg-white text-black' : 'bg-gray-800 text-white`}>
         {children}
       </div>
     </div>

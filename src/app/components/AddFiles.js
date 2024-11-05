2ff7720e516afb65c914462d842fc6a8012e7329
TaopikQM <taopik61318@gmail.com>
2024-11-05 08:20:32 +0700

    Update AddFiles.js

--- src/app/components/AddFiles.js
+++ src/app/components/AddFiles.js
@@ -119,6 +119,7 @@ const AddFiles = () => {
       {/* Submit Button */}
         <button
           type="handleSubmit"
+          onClick={handleSubmit}
           disabled={isLoading} // Tambahkan properti disabled berdasarkan isLoading
           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
